060846516ee18c4fd4b0398eba1f995eeaa8d067
Ducko <duckodas@gmail.com>
2023-09-12 18:49:46 +0200

    Update wishlist.js

--- wishlist.js
+++ wishlist.js
@@ -2,6 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const addWishForm = document.getElementById("addWishForm");
   const wishlistItems = document.getElementById("wishlistItems");
 
+  // Load wishes from local storage on page load
+  loadWishes();
+
   addWishForm.addEventListener("submit", function (e) {
     e.preventDefault();
     const wishName = document.getElementById("wishName").value;
@@ -9,12 +12,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const wishLink = document.getElementById("wishLink").value;
 
     if (wishName && wishPrice && wishLink) {
-      const listItem = document.createElement("li");
-      listItem.innerHTML = `
-                <span>${wishName} - ${wishPrice}</span>
-                <a href="${wishLink}" target="_blank">Buy</a>
-            `;
-      wishlistItems.appendChild(listItem);
+      // Create a new wish object
+      const newWish = {
+        name: wishName,
+        price: wishPrice,
+        link: wishLink,
+      };
+
+      // Save the wish to local storage
+      saveWish(newWish);
+
+      // Add the wish to the list
+      appendWishToList(newWish);
 
       // Clear the form inputs
       document.getElementById("wishName").value = "";
@@ -22,4 +31,27 @@ document.addEventListener("DOMContentLoaded", function () {
       document.getElementById("wishLink").value = "";
     }
   });
+
+  // Function to save a wish to local storage
+  function saveWish(wish) {
+    let wishes = JSON.parse(localStorage.getItem("wishes")) || [];
+    wishes.push(wish);
+    localStorage.setItem("wishes", JSON.stringify(wishes));
+  }
+
+  // Function to load wishes from local storage
+  function loadWishes() {
+    const wishes = JSON.parse(localStorage.getItem("wishes")) || [];
+    wishes.forEach(appendWishToList);
+  }
+
+  // Function to display a wish in the list
+  function appendWishToList(wish) {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `
+          <span>${wish.name} - ${wish.price}</span>
+          <a href="${wish.link}" target="_blank">Buy</a>
+      `;
+    wishlistItems.appendChild(listItem);
+  }
 });
